23e2c5331e5e7d47d7f5d5709fd858be90aa87bc
guofeng7998 <40385403+guofeng7998@users.noreply.github.com>
2019-12-06 17:49:51 +0800

    Add files via upload

---
+++ Assets/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Jump : MonoBehaviour {
+
+	public void OnButtonClick()
+	{
+		SceneManager.LoadScene (1);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/UIheader.cs
@@ -0,0 +1,43 @@
+﻿/*using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class UIHeader:MonoBehaviour	
+{
+	public Text head;
+	public string str="Cytus";
+	str
+}*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIheader : MonoBehaviour 
+{
+	Image fillImage;
+	bool isStartTime = false;
+	float startTime = 0;
+	float curTime;
+
+	void Start () {
+		fillImage = GameObject.Find("UICanvas/UI_Image/background").GetComponent<Image>();
+		fillImage.fillAmount = 1;
+	}
+
+	void Update ()
+	{
+		curTime = Time.time;
+		if (isStartTime && curTime-startTime<10)
+		{
+			fillImage.fillAmount = (startTime + 10 - curTime) / 10;
+		}
+
+	}
+
+
+	public void TheClick()
+	{
+		isStartTime = true;
+		startTime = Time.time;
+		print("STart");
+	}
+}
